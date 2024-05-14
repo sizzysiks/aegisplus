a85cf4cde8c67eb98980749eeb6dcf658d95278f
Antino Kila <87396903+sizzysiks@users.noreply.github.com>
2024-05-14 11:23:05 +0100

    Delete assets directory

--- assets/LazyImage-B20IC0Hc.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-DIdLB8O5.js";function m({src:t,alt:r}){return a.jsx("img",{src:t,alt:r})}export{m as default};
--- assets/LazyVideo-C5Lh4cX0.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DIdLB8O5.js";function a({src:o,alt:r}){return e.jsx("div",{className:"project-video-wrapper",children:e.jsx("video",{src:o,autoPlay:!0,loop:!0})})}export{a as default};
