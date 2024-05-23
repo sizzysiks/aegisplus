3a8f8f69d1d012b312e7a1b946518ed53c3c6d1c
Antino Kila <87396903+sizzysiks@users.noreply.github.com>
2024-05-23 12:50:57 +0100

    Add files via upload

---
+++ build/assets/LazyVideo-BGBuuIth.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DaL2jett.js";function a({src:o,alt:r}){return e.jsx("div",{className:"project-video-wrapper",children:e.jsx("video",{src:o,autoPlay:!0,loop:!0})})}export{a as default};
