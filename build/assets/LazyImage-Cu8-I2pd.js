5a6aac2d7f3855eee4271de7be7a7178353694bf
Antino Kila <87396903+sizzysiks@users.noreply.github.com>
2024-06-03 12:54:17 +0100

    Add files via upload

---
+++ build/assets/LazyImage-Cu8-I2pd.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-B5EAhwnZ.js";function m({src:t,alt:r}){return a.jsx("img",{src:t,alt:r})}export{m as default};
---
+++ build/assets/LazyVideo-_vO4K2Av.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B5EAhwnZ.js";function a({src:o,alt:r}){return e.jsx("div",{className:"project-video-wrapper",children:e.jsx("video",{src:o,autoPlay:!0,loop:!0})})}export{a as default};
